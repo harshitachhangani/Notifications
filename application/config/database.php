5feda5c5c63aef483bff3147ca566c122c99e730
Harshita Chhangani <harshitachhangani05@gmail.com>
2022-07-05 14:18:09 +0530

    minor commit

--- application/config/database.php
+++ application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => 'l',
 	'username' => 'root',
-	'password' => '',
+	'password' => 'localhost',
 	'database' => 'ems',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
